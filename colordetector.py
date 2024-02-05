120388056592afe73680c032659f1604a8d4055f
leumij <cjimuel74@gmail.com>
2024-02-06 00:29:44 +0800

    frane

--- colordetector.py
+++ colordetector.py
@@ -3,7 +3,11 @@
 import cv2
 import numpy as np
 
+frameWidth = 640
+frameHeight = 480
 cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
 
 while True:
     ret, frame = cap.read()
